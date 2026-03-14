e5104a38f774002c1131c6c812de430cfad5a059
Poornesh H S <poorneshhskutsa@gmail.com>
2026-03-14 13:35:56 +0530

    feat(quotation): add quotation creating feature

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 
 import contactRoutes from "./routes/contact.routes.js";
 import experienceRoutes from "./routes/experience.routes.js";
+import quotationRoutes from "./routes/quotation.routes.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use("/api/contact", contactRoutes);
 app.use("/api/experience", experienceRoutes);
+app.use("/api/quotation", quotationRoutes);
 
 app.get("/", (req, res) => {
   res.send("API running successfully 🚀");
